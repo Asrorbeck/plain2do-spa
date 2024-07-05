20f616ed23c1e10b6faac2fe4ead9ea8248009a7
Asrorbek Tursunpulatov <94453043+Asrorbeck@users.noreply.github.com>
2024-07-05 17:01:19 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <>
+            <h1
+                style={{
+                    fontFamily: "Arial",
+                    position: "absolute",
+                    left: "50%",
+                    top: "50%",
+                    transform: "translate(-50%, -50%)",
+                }}
+            >
+                Hello, Asrorbek Tursunpulatov
+            </h1>
+        </>
+    );
+}
+
+export default App;
